178bd1666797ce83137b5bd7e9fbffad5572678f
fz6m <fz6meng@gmail.com>
2021-04-27 00:25:15 +0800

    feat: upgrade plugin 🍭

--- plugins/bot_pcr_fortune.py
+++ plugins/bot_pcr_fortune.py
@@ -1,6 +1,25 @@
 # -*- coding:utf-8 -*-
 
-RESOURCES_BASE_PATH = './resources/pcr-fortune'
+import base64
+import datetime
+import os
+import random
+import re
+from enum import Enum
+
+from botoy import Action, GroupMsg
+from botoy.collection import MsgTypes
+from botoy.decorators import ignore_botself, these_msgtypes
+from dateutil.parser import parse
+from PIL import Image, ImageDraw, ImageFont
+
+try:
+    import ujson as json
+except Exception:
+    import json
+
+# ==========================================
+RESOURCES_BASE_PATH = "./resources/pcr-fortune"
 
 # ==========================================
 
@@ -8,41 +27,28 @@ RESOURCES_BASE_PATH = './resources/pcr-fortune'
 blockGroupNumber = []
 # 触发命令列表
 commandList = [
-    '今日人品', '今日运势', '抽签', '人品', '运势', 
-    'kkr签', '妈签',
-    'xcw签', '炼签',
-    'kyaru签', '臭鼬签'
+    "今日人品",
+    "今日运势",
+    "抽签",
+    "人品",
+    "运势",
+    "kkr签",
+    "妈签",
+    "xcw签",
+    "炼签",
+    "kyaru签",
+    "臭鼬签",
 ]
 # 是否开启泳装签
 swimsuitModel = False
 
 # ==========================================
 
-from types import coroutine
-from iotbot import Action, GroupMsg
-from PIL import Image, ImageDraw, ImageFont
 
-import base64
-import os
-import re
-import random
-from enum import Enum
-import datetime
-from dateutil.parser import parse
+bot = Action(int(os.getenv("BOTQQ")))
 
-try:
-    import ujson as json
-except:
-    import json
 
-# ==========================================
-
-bot = Action(
-    qq_or_bot = int(os.getenv('BOTQQ')),
-    queue = True,
-    queue_delay = 0.5
-)
-
+@ignore_botself
 def receive_group_msg(ctx: GroupMsg):
     userGroup = ctx.FromGroupId
 
@@ -59,59 +65,54 @@ def receive_group_msg(ctx: GroupMsg):
 
 
 class Model(Enum):
-    
-    ALL = '_all'
 
-    BLURRY = '_blurry'
+    ALL = "_all"
 
-    SEND_AT = '_send_at'
+    BLURRY = "_blurry"
 
-    SEND_DEFAULT = '_send_default'
+    SEND_AT = "_send_at"
+
+    SEND_DEFAULT = "_send_default"
 
 
 class Status(Enum):
 
-    SUCCESS = '_success'
+    SUCCESS = "_success"
 
-    FAILURE = '_failure'
+    FAILURE = "_failure"
 
 
-
-class Tools():
-
+class Tools:
     @staticmethod
     def textOnly(msgType):
-        return True if msgType == 'TextMsg' else False
-
+        return msgType == MsgTypes.TextMsg
 
     @staticmethod
     def atOnly(msgType):
-        return True if msgType == 'AtMsg' else False
+        return msgType == MsgTypes.AtMsg
 
     @staticmethod
     def writeFile(p, content):
-        with open(p, 'w', encoding = 'utf-8') as f:
+        with open(p, "w", encoding="utf-8") as f:
             f.write(content)
-
 
     @staticmethod
     def readFileByLine(p):
         if not os.path.exists(p):
             return Status.FAILURE
-        with open(p, 'r', encoding = 'utf-8') as f:
+        with open(p, "r", encoding="utf-8") as f:
             return f.readlines()
-
 
     @staticmethod
     def readJsonFile(p):
         if not os.path.exists(p):
             return Status.FAILURE
-        with open(p, 'r', encoding = 'utf-8') as f:
+        with open(p, "r", encoding="utf-8") as f:
             return json.loads(f.read())
 
     @staticmethod
     def writeJsonFile(p, content):
-        with open(p, 'w', encoding = 'utf-8') as f:
+        with open(p, "w", encoding="utf-8") as f:
             f.write(json.dumps(content))
         return Status.SUCCESS
 
@@ -119,56 +120,49 @@ class Tools():
     def readFileContent(p):
         if not os.path.exists(p):
             return Status.FAILURE
-        with open(p, 'r', encoding = 'utf-8') as f:
+        with open(p, "r", encoding="utf-8") as f:
             return f.read().strip()
-
 
     @staticmethod
     def readPictureFile(picPath):
         if not os.path.exists(picPath):
             return Status.FAILURE
-        with open(picPath, 'rb') as f:
+        with open(picPath, "rb") as f:
             return f.read()
-
 
     @classmethod
     def base64conversion(cls, picPath):
         picByte = cls.readPictureFile(picPath)
         if picByte == Status.FAILURE:
-            raise Exception('图片文件不存在！')
-        return str(base64.b64encode(picByte), encoding = 'utf-8')
-
+            raise Exception("图片文件不存在！")
+        return str(base64.b64encode(picByte), encoding="utf-8")
 
     @classmethod
-    def sendPictures(cls, userGroup, picPath, bot, standardization = True, content = '', atUser = 0):
+    def sendPictures(
+        cls, userGroup, picPath, bot: Action, standardization=True, content="", atUser=0
+    ):
         if standardization:
-            content = str(content) + '[PICFLAG]'
-        bot.send_group_pic_msg(
-            toUser = int(userGroup),
-            picBase64Buf = cls.base64conversion(picPath),
-            atUser = int(atUser),
-            content = content
+            content = str(content) + "[PICFLAG]"
+        bot.sendGroupPic(
+            userGroup,
+            picBase64Buf=cls.base64conversion(picPath),
+            atUser=atUser,
+            content=content,
         )
-    
-    @staticmethod
-    def sendText(userGroup, msg, bot, model = Model.SEND_DEFAULT, atQQ = ''):
-        if msg != '' and msg != Status.FAILURE:
-            if model == Model.SEND_DEFAULT:
-                bot.send_group_text_msg(
-                    toUser = int(userGroup),
-                    content = str(msg)
-                )
-            if model == Model.SEND_AT:
-                if atQQ == '':
-                    raise Exception('没有指定 at 的人！')
-                at = f'[ATUSER({atQQ})]\n'
-                bot.send_group_text_msg(
-                    toUser = int(userGroup),
-                    content = at + str(msg)
-                )
 
     @staticmethod
-    def commandMatch(msg, commandList, model = Model.ALL):
+    def sendText(userGroup, msg, bot, model=Model.SEND_DEFAULT, atQQ=""):
+        if msg != "" and msg != Status.FAILURE:
+            if model == Model.SEND_DEFAULT:
+                bot.sendGroupText(userGroup, content=str(msg))
+            if model == Model.SEND_AT:
+                if atQQ == "":
+                    raise Exception("没有指定 at 的人！")
+                at = f"[ATUSER({atQQ})]\n"
+                bot.sendGroupText(userGroup, content=at + str(msg))
+
+    @staticmethod
+    def commandMatch(msg, commandList, model=Model.ALL):
         if model == Model.ALL:
             for c in commandList:
                 if c == msg:
@@ -186,29 +180,31 @@ class Tools():
 
     @staticmethod
     def atQQ(userQQ):
-        return f'[ATUSER({userQQ})]\n'
+        return f"[ATUSER({userQQ})]\n"
 
 
-class TimeUtils():
+class TimeUtils:
 
-    DAY = 'day'
+    DAY = "day"
 
-    HOUR = 'hour'
+    HOUR = "hour"
 
-    MINUTE = 'minute'
+    MINUTE = "minute"
 
-    SECOND = 'second'
+    SECOND = "second"
 
-    ALL = 'all'
+    ALL = "all"
 
     @staticmethod
     def getTheCurrentTime():
-        nowDate = str(datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d'))
+        nowDate = str(datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d"))
         return nowDate
 
     @staticmethod
     def getAccurateTimeNow():
-        nowDate = str(datetime.datetime.strftime(datetime.datetime.now(),'%Y-%m-%d/%H:%M:%S'))
+        nowDate = str(
+            datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d/%H:%M:%S")
+        )
         return nowDate
 
     @classmethod
@@ -220,7 +216,7 @@ class TimeUtils():
 
     @staticmethod
     def getTheCurrentHour():
-        return int(str(datetime.datetime.strftime(datetime.datetime.now(),'%H')))
+        return int(str(datetime.datetime.strftime(datetime.datetime.now(), "%H")))
 
     @classmethod
     def calculateTheElapsedTimeCombination(cls, lastTime):
@@ -232,9 +228,11 @@ class TimeUtils():
 
     @staticmethod
     def replaceHourMinuteAndSecond(parameterList, msg):
-        return (msg.replace(r'{hour}', str(parameterList[0]))
-                    .replace(r'{minute}', str(parameterList[1]))
-                    .replace(r'{second}', str(parameterList[2])))
+        return (
+            msg.replace(r"{hour}", str(parameterList[0]))
+            .replace(r"{minute}", str(parameterList[1]))
+            .replace(r"{second}", str(parameterList[2]))
+        )
 
     @classmethod
     def getTimeDifference(cls, original, model):
@@ -245,8 +243,8 @@ class TimeUtils():
             return {
                 cls.DAY: int((b - a).days),
                 cls.HOUR: int(seconds / 3600),
-                cls.MINUTE: int((seconds % 3600) / 60), # The rest
-                cls.SECOND: int(seconds % 60) # The rest
+                cls.MINUTE: int((seconds % 3600) / 60),  # The rest
+                cls.SECOND: int(seconds % 60),  # The rest
             }
         if model == cls.DAY:
             b = parse(cls.getTheCurrentTime())
@@ -259,31 +257,29 @@ class TimeUtils():
 
 class PcrFortuneModel(Enum):
 
-    KYARU = '2'
+    KYARU = "2"
 
-    XCW = '27'
+    XCW = "27"
 
-    M = '41'
+    M = "41"
 
-    DEFAULT = 'default'
+    DEFAULT = "default"
 
 
-class Swimsuit():
+class Swimsuit:
 
-    prohibitedValue = [
-        3, 25, [49, 52], 59
-    ]
+    prohibitedValue = [3, 25, [49, 52], 59]
 
     @classmethod
     def swimsuitRecognition(cls, path):
         # Get the serial number in the path
         try:
-            number = int(re.search('frame_(\d)', path).group(1))
+            number = int(re.search("frame_(\d)", path).group(1))
         except:
             raise Exception('The image name does not conform to the format "frame_1" !')
         # Identify whether it is within the prohibited range
         for i in cls.prohibitedValue:
-            if isinstance(i, list) and ( number in [ j for j in range(i[0], i[1] + 1)] ):
+            if isinstance(i, list) and (number in [j for j in range(i[0], i[1] + 1)]):
                 return True
             if isinstance(i, int) and number == i:
                 return True
@@ -297,63 +293,64 @@ def handlingMessages(msg, bot, userGroup, userQQ):
         if testUse(userQQ) == Status.SUCCESS:
             model = PcrFortuneModel.DEFAULT
             # kkr
-            if msg.find('kkr') != -1 or msg.find('妈') != -1:
+            if msg.find("kkr") != -1 or msg.find("妈") != -1:
                 model = PcrFortuneModel.M
             # kyaru
-            if msg.find('kyaru') != -1 or msg.find('臭鼬') != -1:
+            if msg.find("kyaru") != -1 or msg.find("臭鼬") != -1:
                 model = PcrFortuneModel.KYARU
             # xcw
-            if msg.find('xcw') != -1 or msg.find('炼') != -1:
+            if msg.find("xcw") != -1 or msg.find("炼") != -1:
                 model = PcrFortuneModel.XCW
             # Plot
             outPath = drawing(model, userQQ)
             # Send a message
             Tools.sendPictures(
-                userGroup = userGroup,
-                picPath = outPath,
-                bot = bot,
-                content = Tools.atQQ(userQQ)
+                userGroup=userGroup,
+                picPath=outPath,
+                bot=bot,
+                content=Tools.atQQ(userQQ),
             )
             return
 
 
 def testUse(userQQ):
-    p = f'{RESOURCES_BASE_PATH}/user/{userQQ}.json'
-    dir = f'{RESOURCES_BASE_PATH}/user'
+    p = f"{RESOURCES_BASE_PATH}/user/{userQQ}.json"
+    dir = f"{RESOURCES_BASE_PATH}/user"
     Tools.checkFolder(dir)
     content = Tools.readJsonFile(p)
     if content == Status.FAILURE:
-        userStructure = {
-            'time': TimeUtils.getTheCurrentTime()
-        }
+        userStructure = {"time": TimeUtils.getTheCurrentTime()}
         Tools.writeJsonFile(p, userStructure)
         return Status.SUCCESS
-    interval = TimeUtils.getTimeDifference(content['time'], TimeUtils.DAY)
+    interval = TimeUtils.getTimeDifference(content["time"], TimeUtils.DAY)
     if interval >= 1:
-        content['time'] = TimeUtils.getTheCurrentTime()
+        content["time"] = TimeUtils.getTheCurrentTime()
         Tools.writeJsonFile(p, content)
         return Status.SUCCESS
     return Status.FAILURE
 
+
 def copywriting():
-    p = f'{RESOURCES_BASE_PATH}/fortune/copywriting.json'
+    p = f"{RESOURCES_BASE_PATH}/fortune/copywriting.json"
     content = Tools.readJsonFile(p)
-    return random.choice(content['copywriting'])
+    return random.choice(content["copywriting"])
+
 
 def getTitle(structure):
-    p = f'{RESOURCES_BASE_PATH}/fortune/goodLuck.json'
+    p = f"{RESOURCES_BASE_PATH}/fortune/goodLuck.json"
     content = Tools.readJsonFile(p)
-    for i in content['types_of']:
-        if i['good-luck'] == structure['good-luck']:
-            return i['name']
-    raise Exception('Configuration file error')
+    for i in content["types_of"]:
+        if i["good-luck"] == structure["good-luck"]:
+            return i["name"]
+    raise Exception("Configuration file error")
+
 
 def drawing(model, userQQ):
     fontPath = {
-        'title': f'{RESOURCES_BASE_PATH}/font/Mamelon.otf',
-        'text': f'{RESOURCES_BASE_PATH}/font/sakura.ttf'
+        "title": f"{RESOURCES_BASE_PATH}/font/Mamelon.otf",
+        "text": f"{RESOURCES_BASE_PATH}/font/sakura.ttf",
     }
-    imgPath = ''
+    imgPath = ""
     # When not a specific picture
     if model == PcrFortuneModel.DEFAULT:
         randomPath = randomBasemap()
@@ -367,63 +364,79 @@ def drawing(model, userQQ):
             # Judge whether it is reasonable
             if Swimsuit.swimsuitRecognition(randomPath):
                 # If it is still a swimsuit three times, the kkr base map is taken by default
-                imgPath = f'{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.M.value}.jpg'
+                imgPath = (
+                    f"{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.M.value}.jpg"
+                )
             else:
                 imgPath = randomPath
         else:
             imgPath = randomPath
     # kkr
     if model == PcrFortuneModel.M:
-        imgPath = f'{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.M.value}.jpg'
+        imgPath = f"{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.M.value}.jpg"
     # kyaru
     if model == PcrFortuneModel.KYARU:
-        imgPath = f'{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.KYARU.value}.jpg'
+        imgPath = f"{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.KYARU.value}.jpg"
     # xcw
     if model == PcrFortuneModel.XCW:
-        imgPath = f'{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.XCW.value}.jpg'
+        imgPath = f"{RESOURCES_BASE_PATH}/img/frame_{PcrFortuneModel.XCW.value}.jpg"
     img = Image.open(imgPath)
     # Draw title
     draw = ImageDraw.Draw(img)
     text = copywriting()
     title = getTitle(text)
-    text = text['content']
+    text = text["content"]
     font_size = 45
-    color = '#F5F5F5'
+    color = "#F5F5F5"
     image_font_center = (140, 99)
-    ttfront = ImageFont.truetype(fontPath['title'], font_size)
+    ttfront = ImageFont.truetype(fontPath["title"], font_size)
     font_length = ttfront.getsize(title)
-    draw.text((image_font_center[0]-font_length[0]/2, image_font_center[1]-font_length[1]/2),
-                title, fill=color,font=ttfront)
+    draw.text(
+        (
+            image_font_center[0] - font_length[0] / 2,
+            image_font_center[1] - font_length[1] / 2,
+        ),
+        title,
+        fill=color,
+        font=ttfront,
+    )
     # Text rendering
     font_size = 25
-    color = '#323232'
+    color = "#323232"
     image_font_center = [140, 297]
-    ttfront = ImageFont.truetype(fontPath['text'], font_size)
+    ttfront = ImageFont.truetype(fontPath["text"], font_size)
     result = decrement(text)
     if not result[0]:
-        return 
+        return
     textVertical = []
     for i in range(0, result[0]):
         font_height = len(result[i + 1]) * (font_size + 4)
         textVertical = vertical(result[i + 1])
-        x = int(image_font_center[0] + (result[0] - 2) * font_size / 2 + 
-                (result[0] - 1) * 4 - i * (font_size + 4))
+        x = int(
+            image_font_center[0]
+            + (result[0] - 2) * font_size / 2
+            + (result[0] - 1) * 4
+            - i * (font_size + 4)
+        )
         y = int(image_font_center[1] - font_height / 2)
-        draw.text((x, y), textVertical, fill = color, font = ttfront)
+        draw.text((x, y), textVertical, fill=color, font=ttfront)
     # Save
     outPath = exportFilePath(imgPath, userQQ)
     img.save(outPath)
     return outPath
 
+
 def exportFilePath(originalFilePath, userQQ):
-    outPath = originalFilePath.replace('/img/', '/out/').replace('frame', str(userQQ))
-    dirPath = f'{RESOURCES_BASE_PATH}/out'
+    outPath = originalFilePath.replace("/img/", "/out/").replace("frame", str(userQQ))
+    dirPath = f"{RESOURCES_BASE_PATH}/out"
     Tools.checkFolder(dirPath)
     return outPath
 
+
 def randomBasemap():
-    p = f'{RESOURCES_BASE_PATH}/img'
-    return p + '/' + random.choice(os.listdir(p))
+    p = f"{RESOURCES_BASE_PATH}/img"
+    return p + "/" + random.choice(os.listdir(p))
+
 
 def decrement(text):
     length = len(text)
@@ -437,23 +450,30 @@ def decrement(text):
         length -= cardinality
     result.append(numberOfSlices)
     # Optimize for two columns
-    space = ' '
+    space = " "
     length = len(text)
     if numberOfSlices == 2:
         if length % 2 == 0:
             # even
             fillIn = space * int(9 - length / 2)
-            return [numberOfSlices, text[:int(length / 2)] + fillIn, fillIn + text[int(length / 2):]]
+            return [
+                numberOfSlices,
+                text[: int(length / 2)] + fillIn,
+                fillIn + text[int(length / 2) :],
+            ]
         else:
             # odd number
             fillIn = space * int(9 - (length + 1) / 2)
-            return [numberOfSlices, text[:int((length + 1) / 2)] + fillIn,
-                                    fillIn + space + text[int((length + 1) / 2):]]
+            return [
+                numberOfSlices,
+                text[: int((length + 1) / 2)] + fillIn,
+                fillIn + space + text[int((length + 1) / 2) :],
+            ]
     for i in range(0, numberOfSlices):
         if i == numberOfSlices - 1 or numberOfSlices == 1:
-            result.append(text[i * cardinality:])
+            result.append(text[i * cardinality :])
         else:
-            result.append(text[i * cardinality:(i + 1) * cardinality])
+            result.append(text[i * cardinality : (i + 1) * cardinality])
     return result
 
 
@@ -461,4 +481,4 @@ def vertical(str):
     list = []
     for s in str:
         list.append(s)
-    return '\n'.join(list)
+    return "\n".join(list)
